15428d31813695523ac18706f345beaa8be59fae
Julien Del Rio <julien.delrio@gmail.com>
2021-05-21 22:42:29 +0200

    Nice bot who say hello

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -2,15 +2,25 @@ const Discord = require('discord.js')
 
 const discord = new Discord.Client()
 
-exports.start = function (){
+var botId;
+
+exports.start = function () {
     discord.login('ODQ1MjYyMjE0Njg4NjY5NzI3.YKeZyA.cXf5yHDjaqfC8ivgRkHq3YeqIGI')
 }
 
 discord.on('ready', function () {
-    console.log("Je suis connecté !")
+    console.log(`Je suis connecté : ${discord.user.id}`)
 })
 discord.on('message', message => {
+    console.log(`New message from ${message.member} : ${message.content}`)
+    dispatch(message);
+})
+
+function dispatch(message) {
     if (message.content === 'ping') {
         message.reply('pong !')
+    } else if (message.content.match(`bonjour <@!${discord.user.id}>`)) {
+        console.log(JSON.stringify(message, null, 4))
+        message.channel.send(`Bonjour ${message.member} !`);
     }
-})
+}
