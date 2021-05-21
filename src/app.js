02e8b3b9b3f6cf87532e2fe170daa44b940b8509
Julien Del Rio <julien.delrio@gmail.com>
2021-05-21 14:26:41 +0200

    Test Example submit https://cloud.google.com/appengine/docs/standard/nodejs/building-app/updating-web-service?hl=fr

--- src/app.js
+++ src/app.js
@@ -1,26 +1,31 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 'use strict';
 
-// [START gae_node_request_example]
 const express = require('express');
+const path = require(`path`);
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// App config
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+// Root listener
 app.get('/', (req, res) => {
     res.status(200).send('Hello, world!').end();
+});
+
+// Add Submit listener
+app.get('/submit', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/submit.html'));
+});
+
+app.post('/submit', (req, res) => {
+    console.log({
+        name: req.body.name,
+        message: req.body.message
+    });
+    res.send('Thanks for your message!');
 });
 
 // Start the server
