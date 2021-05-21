7912ba3f99cffb197becdaad033d99830d206888
Julien Del Rio <julien.delrio@gmail.com>
2021-05-21 22:12:12 +0200

    Move bot code

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -3,14 +3,14 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 
 exports.start = function (){
-    bot.on('ready', function () {
-        console.log("Je suis connecté !")
-    })
-    bot.on('message', message => {
-        if (message.content === 'ping') {
-            message.reply('pong !')
-        }
-    })
-
     bot.login('ODQ1MjYyMjE0Njg4NjY5NzI3.YKeZyA.cXf5yHDjaqfC8ivgRkHq3YeqIGI')
 }
+
+bot.on('ready', function () {
+    console.log("Je suis connecté !")
+})
+bot.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong !')
+    }
+})
