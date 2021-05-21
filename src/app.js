760252e0520864d8e14a49c04e6f9ca2deea6d74
Julien Del Rio <julien.delrio@gmail.com>
2021-05-21 17:13:54 +0200

    Listen on post and print source IP

--- src/app.js
+++ src/app.js
@@ -7,12 +7,25 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // App config
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Routes
 // Root listener
 app.get('/', (req, res) => {
     res.status(200).send('Hello, world!').end();
+});
+
+// Api
+// ----- Interactions
+app.post('/api/interactions', (req, res) => {
+    var reqIp = req.headers['x-appengine-user-ip'];
+    if(!reqIp) reqIp = req.ip
+    console.log(`Request received from ${reqIp}`);
+    res.setHeader('content-type', 'application/json');
+    let payload = {
+        "type": 1
+    }
+    res.status(200).send(JSON.stringify(payload, null, 4)).end();
 });
 
 // Start the server
