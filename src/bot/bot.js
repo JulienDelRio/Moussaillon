6f4da96d579946be2dbcfa3a4e80833dae8d7ca8
Julien Del Rio <julien.delrio@gmail.com>
2021-06-03 22:16:12 +0200

    Load Data

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,8 +1,12 @@
+'use strict';
+
+const fs = require('fs');
 const Discord = require('discord.js')
 
 const discord = new Discord.Client()
 
 var botId;
+var data;
 
 exports.start = function () {
     discord.login('ODQ1MjYyMjE0Njg4NjY5NzI3.YKeZyA.cXf5yHDjaqfC8ivgRkHq3YeqIGI')
@@ -10,6 +14,9 @@ exports.start = function () {
 
 discord.on('ready', function () {
     console.log(`Je suis connecté : ${discord.user.id}`)
+
+    let rawdata = fs.readFileSync('./data/data.json');
+    data = JSON.parse(rawdata);
 })
 discord.on('message', message => {
     console.log(`New message from ${message.member} : ${message.content}`)
@@ -17,10 +24,22 @@ discord.on('message', message => {
 })
 
 function dispatch(message) {
+    if (message.author.bot) {
+        console.log("Message from the bot himself.")
+        return;
+    }
+    if (message.content.startsWith("=")){
+        console.log("It's a command.")
+        return;
+    }
     if (message.content === 'ping') {
         message.reply('pong !')
-    } else if (message.content.match(`bonjour <@!${discord.user.id}>`)) {
-        console.log(JSON.stringify(message, null, 4))
-        message.channel.send(`Bonjour ${message.member} !`);
+        return;
     }
+    if (message.content.match(`bonjour <@!${discord.user.id}>`)) {
+        message.channel.send(`Bonjour ${message.member} !`);
+        return;
+    }
+
+    console.log("Not dispatched")
 }
