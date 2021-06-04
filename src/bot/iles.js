8017a8b90d15e58df5bb59fec356ac4f90b68fe6
Julien Del Rio <julien.delrio@gmail.com>
2021-06-04 15:21:45 +0200

    Add message for unknown island

--- src/bot/iles.js
+++ src/bot/iles.js
@@ -1,9 +1,14 @@
 'use strict';
 
 exports.handle = function (message, commandParams) {
+    let lowerCommandParams = commandParams.toLowerCase()
+    let found = false;
     data.iles.forEach(function (ile) {
-        if (ile.name == commandParams){
+        if (ile.name.toLowerCase() == lowerCommandParams){
             message.channel.send(ile.name + " dans la mer " + ile.sea)
+            found = true;
         }
     })
+    if (!found)
+        message.channel.send("Cette ile est inconnue au bataillon. Retente ta chance matelot.")
 }
