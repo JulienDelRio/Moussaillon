673fcbe0c7d1f1db001c3709023294e913da1d65
Julien Del Rio <julien.delrio@gmail.com>
2021-06-07 22:56:28 +0200

    Improve island description with a log of new fields

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -26,45 +26,47 @@ discord.on('message', message => {
 })
 
 function dispatch(message) {
-    let testChannel = config.testChannels.includes(message.channel.id);
-    console.log(message.channel.name + " is test ? " + testChannel)
-    let allowedChannel = config.allowedChannels.includes(message.channel.id);
-    console.log(message.channel.name + " is allowed ? " + allowedChannel)
-    if (!(allowedChannel || testChannel)) {
-        console.log(`Wrong chan ${message.channel.name} (${message.channel.id})`);
-        return
-    }
-    if (message.author.bot) {
-        console.log("Message from the bot himself.")
-        return;
-    }
-    if (message.content.startsWith(config.prefix)) {
-        let command = message.content.substring(1).split(" ")[0]
-        let commandParams = message.content.substring(1 + 1 + command.length);
+    try {
 
-        switch (command) {
-            case "test":
-                if (testChannel)
-                    testdiscordapi.handle(message, commandParams)
-                else
-                    console.log("test not allowed in " + message.channel.name);
-                break
-            case "ile":
-                islands.handle(message, commandParams)
-                break
-            default:
-                console.log("It's a command : " + command)
+        let testChannel = config.testChannels.includes(message.channel.id);
+        let allowedChannel = config.allowedChannels.includes(message.channel.id);
+        if (!(allowedChannel || testChannel)) {
+            console.log(`Wrong chan ${message.channel.name} (${message.channel.id})`);
+            return
         }
-        return;
-    }
-    if (message.content === 'ping') {
-        message.reply('pong !')
-        return;
-    }
-    if (message.content.toLowerCase().match(`bonjour <@!${discord.user.id}>`.toLowerCase())) {
-        message.channel.send(`Bonjour ${message.member} !`);
-        return;
-    }
+        if (message.author.bot) {
+            console.log("Message by a bot.")
+            return;
+        }
+        if (message.content.startsWith(config.prefix)) {
+            let command = message.content.substring(1).split(" ")[0].toLowerCase();
+            let commandParams = message.content.substring(1 + 1 + command.length);
 
-    console.log("Not dispatched")
+            switch (command) {
+                case "test":
+                    if (testChannel)
+                        testdiscordapi.handle(message, commandParams)
+                    else
+                        console.log("test not allowed in " + message.channel.name);
+                    break
+                case "ile":
+                    islands.handle(message, commandParams)
+                    break
+                default:
+                    console.log("It's a command not found : " + command)
+            }
+            return;
+        }
+        if (message.content === 'ping') {
+            message.reply('pong !')
+            return;
+        }
+        if (message.content.toLowerCase().match(`bonjour <@!${discord.user.id}>`.toLowerCase())) {
+            message.channel.send(`Bonjour ${message.member} !`);
+            return;
+        }
+        console.log("Not dispatched : " + message.url)
+    } catch (e) {
+        console.error("error catching : " + e.message, e);
+    }
 }
--- src/bot/islands.js
+++ src/bot/islands.js
@@ -12,12 +12,36 @@ exports.handle = function (message, commandParams) {
     } else {
         data.islands.forEach(function (island) {
             if (island.name.toLowerCase().match(lowerCommandParams)) {
+                // Prepare data
+                let islandName = defaultValuedefaultValue(island.name, "A venir..");
+                let islandSea = defaultValuedefaultValue(island.sea, "A venir..");
+                let npc = defaultValuedefaultValue(island.npc, "A venir..");
+                let commander = defaultValuedefaultValue(island.commander, "A venir..");
+                let cardCode = defaultValuedefaultValue(island.cardCode, "A venir..");
+                let cardName = defaultValuedefaultValue(island.cardName, "A venir..");
+                let routeFrom = defaultValuedefaultValue(island.routeFrom, "A venir...");
+                let routeTo = defaultValuedefaultValue(island.routeTo, "A venir...");
+                let boat = defaultValuedefaultValue(island.boat, "Non........");
+                let claimed = defaultValuedefaultValue(island.claimed, "Personne");
+                let poneglyphe = defaultValuedefaultValue(island.poneglyphe, "Non");
+
+                // Create message
                 const embed = new MoussaillonMessageEmbed()
-                    .setTitle(island.name + " (" + island.sea + ")")
+                    .setAuthor("Commande par " + message.author.username, message.author.avatarURL())
+                    .setTitle(islandName + " (" + islandSea + ")")
                     .setColor(config.embedColor)
-                    .setDescription("Personnage/Lieu de quête : " + island.npc + "\n" +
-                        "Commandant : " + island.commander + "\n" +
-                        "Carte à collectionner : " + island.cardCode + " - " + island.cardName);
+                    .addField("Controlé par", claimed)
+                    .addField("Personnage/Lieu de quête", npc, true)
+                    .addField("Commandant", commander, true)
+                    .addField("Carte à collectionner", cardCode + " - " + cardName, true)
+                    .addField("Vous pouvez y arriver par...", routeFrom, true)
+                    .addField("Vous pouvez allez vers...", routeTo, true)
+                    .addField('\u200b', '\u200b')
+                    .addField("And now... Spoilers...", '\u200b')
+                    .addField("Y'a-t-il un Poneglyphe ?", "||" + poneglyphe + "||", true)
+                    .addField("Un bateau ?", "||" + boat + "||", true);
+
+                // Send message
                 message.channel.send(embed);
                 found = true;
             }
@@ -25,4 +49,11 @@ exports.handle = function (message, commandParams) {
     }
     if (!found)
         message.channel.send("Cette ile est inconnue au bataillon. Retente ta chance matelot.")
+}
+
+function defaultValuedefaultValue(value, defaultValue) {
+    if (!value || value.length === 0)
+        return defaultValue
+    else
+        return value
 }
