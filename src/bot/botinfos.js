93c9980583c3f80987cdb4e2d8f90394c5efef37
Julien Del Rio <julien.delrio@gmail.com>
2021-06-15 14:35:34 +0200

    Ajout d'une fonction changelog

--- src/bot/botinfos.js
+++ src/bot/botinfos.js
@@ -2,10 +2,12 @@
 
 const MoussaillonMessageEmbed = require("./MoussaillonMessageEmbed.js");
 const version = require('./version.json');
+const moussaillonBotPPUrl = "https://cdn.discordapp.com/avatars/845262214688669727/079857eb39dd2161aaca83daaf9982ab.png?size=4096"
 
 exports.isHandled = function (command) {
     switch (command) {
         case "version":
+        case "changelog":
             return true;
         default:
             return false
@@ -17,6 +19,8 @@ exports.handle = function (message) {
     switch (command) {
         case "version":
             handleVersion(message)
+        case "changelog":
+            handleChangelog(message)
         default:
             console.error("Not a good command : " + command)
             return
@@ -25,4 +29,19 @@ exports.handle = function (message) {
 
 function handleVersion(message) {
     message.channel.send("Version : " + version.currentVersion)
+}
+
+function handleChangelog(message) {
+    let lastVersion = version.hystory[0];
+
+    if (lastVersion) {
+        const embed = new MoussaillonMessageEmbed()
+            .setAuthor("Commande par " + message.author.username, message.author.avatarURL())
+            .setTitle("Changelog de Moussaisson")
+            .setThumbnail(moussaillonBotPPUrl)
+            .addField("Version " + lastVersion.version, lastVersion.changelog)
+        message.channel.send(embed);
+    } else {
+        message.channel.send("Il n'y a pas de changelog...")
+    }
 }
