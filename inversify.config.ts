45ac3d50a9928adc6dc8e7bbe5a4e9947deadf93
Julien Del Rio <julien.delrio@gmail.com>
2021-08-16 14:22:39 +0200

    Fix new option in Discord Client constructor

--- inversify.config.ts
+++ inversify.config.ts
@@ -2,13 +2,13 @@ import "reflect-metadata";
 import {Container} from "inversify";
 import {TYPES} from "./src/types";
 import {MoussaillonBot} from "./src/bot/moussaillon-bot";
-import {Client} from "discord.js";
+import {Client, Intents} from "discord.js";
 import {MessageResponder} from "./src/services/commands/message-responder";
 
 let container = new Container();
 
 container.bind<MoussaillonBot>(TYPES.MoussaillonBot).to(MoussaillonBot).inSingletonScope();
-container.bind<Client>(TYPES.Client).toConstantValue(new Client());
+container.bind<Client>(TYPES.Client).toConstantValue(new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS]}));
 container.bind<MessageResponder>(TYPES.MessageResponder).to(MessageResponder).inSingletonScope();
 
 export default container;
